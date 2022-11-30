d1e97e7576e71b1e34641b7f0635618a481eeb3e
Kawa MANMI <kawamam2007@gmail.com>
2022-11-30 15:40:24 +0000

    Level3, Challange for making Favourite name has been done

--- src/ExtractingBabyName.js
+++ src/ExtractingBabyName.js
@@ -1,5 +1,5 @@
-import React from "react";
-import babyNameData from "./data/babyNameData.json";
+import { React, useState } from "react";
+import babyNamesData from "./data/babyNameData.json";
 import "./App.css";
 import { GenderMale } from "react-bootstrap-icons";
 import { GenderFemale } from "react-bootstrap-icons";
@@ -8,7 +8,9 @@ export default function ExtractingBabyName() {
   //   var babyNameSorted = [];
   //   babyNameData.forEach((elm) => babyNameSorted.push(elm.name));
   //   babyNameSorted.sort();
-  babyNameData.sort((a, b) => {
+    const [babyNamesArray, setBabyNamesArray] = useState(babyNamesData);
+
+  babyNamesArray.sort((a, b) => {
     let fa = a.name.toLowerCase(),
       fb = b.name.toLowerCase();
 
@@ -20,6 +22,10 @@ export default function ExtractingBabyName() {
     }
     return 0;
   });
+
+  const [valueSearch, setValueSearch] = useState("");
+  const [favouritesArray, setFavouritesArray] = useState([]);
+
   const symbolGender = (gender) => {
     return gender === "f" ? <GenderMale /> : <GenderFemale />;
   };
@@ -28,11 +34,72 @@ export default function ExtractingBabyName() {
     return gender === "f" ? "btn btn-primary" : "btn btn-danger btn-kawa";
   };
 
+  function onChangeHandler(event) {
+    // console.log(event.target.value);
+    setValueSearch(event.target.value);
+    // console.log(valueSearch);
+    setBabyNamesArray(
+      babyNamesData.filter((babyElm) =>
+        babyElm.name.toLowerCase().includes(event.target.value.toLowerCase())
+      )
+    );
+  }
+
+  const handleFavourite = (index) => {
+    const favouriteBabyName = babyNamesArray[index];
+
+    setBabyNamesArray(
+      babyNamesArray.filter((babyData) => babyData !== babyNamesArray[index])
+    );
+
+    favouritesArray.push(favouriteBabyName);
+    setFavouritesArray([...favouritesArray]);
+    // console.log(favouritesArray);
+  };
+
+  const handleReturnFavourite = (index) => {
+     const returnFavouriteBabyName = favouritesArray[index];
+
+    setFavouritesArray(
+      favouritesArray.filter((babyData) => babyData !== favouritesArray[index])
+    );
+
+     babyNamesArray.push(returnFavouriteBabyName);
+     setBabyNamesArray([...babyNamesArray]);
+    // console.log(favouritesArray);
+  };
   return (
-    <div class="container">
-      {babyNameData.map((babyElm) => (
+    <div className="container">
+      <div className="liveSearch">
+        Live Search
+        <input
+          value={valueSearch}
+          onChange={onChangeHandler}
+          placeholder="Type something to search"
+        />
+      </div>
+      {/* < favouritesArray={favouritesArray} /> */}
+      <div className="favouritesContainer">
+        Favourite Names(Click):
+        {favouritesArray.map((babyElm, index) => (
+          // <button className={elm.sex}>{elm.name}</button>
+          <button
+            className={classNameGender(babyElm.sex)}
+            key={index}
+            onClick={() => handleReturnFavourite(index)}
+          >
+            {babyElm.name} {symbolGender(babyElm.sex)}
+          </button>
+        ))}
+        <hr />
+      </div>
+      {babyNamesArray.map((babyElm, index) => (
         // <button className={elm.sex}>{elm.name}</button>
-        <button className={classNameGender(babyElm.sex)}>
+        <button
+          className={classNameGender(babyElm.sex)}
+          key={index}
+          onClick={() => handleFavourite(index)}
+        >
           {babyElm.name} {symbolGender(babyElm.sex)}
         </button>
       ))}
